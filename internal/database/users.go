b6c601228dddab7ffaa85667df6a47eabf4a5492
heitormbonfim <heitor.mb27@gmail.com>
2025-07-03 22:51:54 -0300

    feat: event attendees in progress

--- internal/database/users.go
+++ internal/database/users.go
@@ -31,3 +31,38 @@ func (m *UserModel) Insert(user *User) error {
 		user.Password,
 	).Scan(&user.Id)
 }
+
+func (m *UserModel) getUser(query string, args ...interface{}) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var user User
+	err := m.DB.QueryRowContext(
+		ctx,
+		query,
+		args...,
+	).Scan(
+		&user.Id,
+		&user.Email,
+		&user.Name,
+		&user.Password,
+	)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (m *UserModel) Get(id int) (*User, error) {
+	query := `--sql
+	SELECT * FROM users WHERE id = $1
+	`
+
+	return m.getUser(query, id)
+}
