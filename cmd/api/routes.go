33c3091f900f7a9f2fa7a46f919ad01d88476291
heitormbonfim <heitor.mb27@gmail.com>
2025-07-07 23:56:30 -0300

    feat: add cors

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"event-app/internal/env"
 	"net/http"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func (app *application) routes() http.Handler {
 	g := gin.Default()
+
+	config := cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
+	g.Use(cors.New(config))
 
 	v1 := g.Group("/api/v1")
 	{
@@ -31,6 +45,15 @@ func (app *application) routes() http.Handler {
 		authGroup.POST("/event/:id/attendee/:userId", app.addAttendeeToEvent)
 		authGroup.DELETE("/events/:id/attendees/:userId", app.deleteAttendeeFromEvent)
 	}
+
+	g.GET("/swagger/*any", func(c *gin.Context) {
+		if c.Request.RequestURI == "/swagger/" {
+			c.Redirect(302, "/swagger/index.html")
+			return
+		}
+		baseURL := env.GetEnvString("BASE_URL", "http://localhost:8080")
+		ginSwagger.WrapHandler(swaggerFiles.Handler, ginSwagger.URL(baseURL+"/swagger/doc.json"))(c)
+	})
 
 	return g
 }
